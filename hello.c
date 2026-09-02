8a3c1e69eaccab8ce53c1e92e9ae2e5103c12eaf
hang <583309681@qq.com>
2026-09-02 21:41:34 +0800

    merge gitea web changes and resolve hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-<<<<<<< HEAD
     printf("Hello, I'm Shu. Code written in VSCode + WSL.\n");
-=======
-    printf("hello, I'm shu. Code written in VSCode + WSL.\n");
->>>>>>> 0033455f9ffb362b006d994f5c0bff1688459693
     printf("This is my first program pushed from WSL.\n");
     return 0;
 }
