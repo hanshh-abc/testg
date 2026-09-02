4698222e613aa7f130193f9e6e30723711814e87
hang <583309681@qq.com>
2026-09-02 21:24:11 +0800

    local changes before pull

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, I'm shu. Code written in VSCode + WSL.\n");
+    printf("Hello, I'm Shu. Code written in VSCode + WSL.\n");
     printf("This is my first program pushed from WSL.\n");
     return 0;
 }
