0033455f9ffb362b006d994f5c0bff1688459693
hang <583309681@qq.com>
2026-09-02 13:21:52 +0000

    更新Hh

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, I'm shu. Code written in VSCode + WSL.\n");
+    printf("hello, I'm shu. Code written in VSCode + WSL.\n");
     printf("This is my first program pushed from WSL.\n");
     return 0;
 }
