ea7d5f42b3c380ec2b1edeb76c0a37ce6c8a26dc
hang <583309681@qq.com>
2026-09-02 14:46:08 +0800

    update hello.c message

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, WSL2 + Gitea + GitHub!\n");
+    printf("Hello, I'm shu. Code written in VSCode + WSL.\n");
     printf("This is my first program pushed from WSL.\n");
     return 0;
 }
